44314df027976607829b1eabd9d71142e0b78e2d
sla602 <sla602@sfu.ca>
2026-02-20 15:32:23 +0900

    Initial Spring Boot project

---
+++ src/main/java/com/mo/mediaodyssey/MediaodysseyApplication.java
@@ -0,0 +1,13 @@
+package com.mo.mediaodyssey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaodysseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaodysseyApplication.class, args);
+	}
+
+}
