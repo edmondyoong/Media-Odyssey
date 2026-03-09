0f63022974660eba8c30d63c069afdf674dfc248
Edmond Yoong <edmond_yoong@sfu.ca>
2026-03-09 12:49:39 -0700

    Fixed buggy Spring Security config

--- src/main/java/com/mo/mediaodyssey/auth/config/SecurityConfig.java
+++ src/main/java/com/mo/mediaodyssey/auth/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.mo.mediaodyssey.auth.security.MOAuthenticationProvider;
@@ -41,5 +43,10 @@ public class SecurityConfig {
                         .loginPage("/auth/login")
                         .permitAll());
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
