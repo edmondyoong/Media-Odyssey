bc9d56ee3dd5bc8a0fb23a008f5d0a3207ec9538
Edmond Yoong <edmond_yoong@sfu.ca>
2026-03-09 18:31:32 -0700

    Allow User creation without email verification for now

--- src/main/java/com/mo/mediaodyssey/auth/model/User.java
+++ src/main/java/com/mo/mediaodyssey/auth/model/User.java
@@ -98,7 +98,7 @@ public class User implements UserDetails {
         this.email = email;
         this.username = email;
         this.password = password;
-        this.isEnabled = false;
+        this.isEnabled = true;
         this.isAccountNonLocked = true;
         this.role = "ROLE_USER";
     }
