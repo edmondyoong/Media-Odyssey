66f90cce571902e3c4f2e7b79b950a0f48c50bea
Neal Lu <neal.lu06@gmail.com>
2026-03-12 13:30:54 -0700

    Fix map-way for web map

--- src/main/java/com/mo/mediaodyssey/layout/controllers/MappingController.java
+++ src/main/java/com/mo/mediaodyssey/layout/controllers/MappingController.java
@@ -35,6 +35,6 @@ public class MappingController {
 
     @GetMapping("/dashboardTab")
     public String navToDashboard() {
-        return "/users/dashboard";
+        return "users/dashboard";
     }
 }
