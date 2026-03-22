c140ef3c1f19af22df30a98150a6bd0ac45a53f7
Neal Lu <neal.lu06@gmail.com>
2026-03-22 16:50:41 -0700

    testing adjustments for fragments and css

--- src/main/java/com/mo/mediaodyssey/layout/controllers/MappingController.java
+++ src/main/java/com/mo/mediaodyssey/layout/controllers/MappingController.java
@@ -2,7 +2,6 @@ package com.mo.mediaodyssey.layout.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
--- src/main/resources/static/layout/js/fragments.js
+++ src/main/resources/static/layout/js/fragments.js
@@ -1,16 +1,31 @@
-/* Functions to fold and unfold side bar + the logo changes everytime actions are made */
+/* === SIDEBAR LOGIC ============
+** Open and close sidebar + Logo will change to when sidebar is toggled.
+===================================
+*/
+
 const logoToggle = document.getElementById("logo");
 const sideBar = document.getElementById("sideBar"); 
+const content = document.getElementById("mainContent"); 
+const formContainer = document.querySelector(".form-container");
+const boardContainer = document.querySelector(".board-container");
 
-const openLogo = "/images/openLogo.svg";
-const closeLogo = "/images/closeLogo.svg"; 
+const openLogo = "/layout/images/openLogo.svg";
+const closeLogo = "/layout/images/closeLogo.svg"; 
 
 logoToggle.addEventListener("click", ()=>{
     sideBar.classList.toggle("collapsed");
 
     if(sideBar.classList.contains("collapsed")) {
         logoToggle.src = closeLogo;
+
+        if(mainContent) mainContent.classList.add("sidebar-collapsed");
+        if(formContainer) formContainer.classList.add("sidebar-collaped");
+        if(boardContainer) boardContainer.classList.add("sidebar-collapsed");
     } else {
         logoToggle.src = openLogo; 
+
+        if(mainContent) mainContent.classList.remove("sidebar-collapsed");
+        if(formContainer) formContainer.classList.remove("sidebar-collaped");
+        if(boardContainer) boardContainer.classList.remove("sidebar-collapsed");
     }
 }); 
