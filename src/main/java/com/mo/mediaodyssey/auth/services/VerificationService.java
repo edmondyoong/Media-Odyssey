5b9d90a1d476f89999a8861058d32abbc7d1c0df
Edmond Yoong <edmond_yoong@sfu.ca>
2026-03-28 22:41:17 -0700

    refactor: Created 'shared' folder for shared logic. EmailService has been moved here. Added GetUserService to obtain the current User based on authentication

--- src/main/java/com/mo/mediaodyssey/auth/services/VerificationService.java
+++ src/main/java/com/mo/mediaodyssey/auth/services/VerificationService.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.mo.mediaodyssey.auth.model.VerificationToken;
 import com.mo.mediaodyssey.auth.repository.UserRepository;
 import com.mo.mediaodyssey.auth.repository.VerificationTokenRepository;
+import com.mo.mediaodyssey.shared.services.EmailService;
 import com.mo.mediaodyssey.auth.dto.ResendVerifyTokenDto;
 import com.mo.mediaodyssey.auth.dto.VerifyTokenDto;
 import com.mo.mediaodyssey.auth.exception.InvalidVerificationTokenException;
---
+++ src/main/java/com/mo/mediaodyssey/shared/services/GetUserService.java
@@ -0,0 +1,32 @@
+package com.mo.mediaodyssey.shared.services;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import com.mo.mediaodyssey.auth.model.User;
+
+@Service
+public class GetUserService {
+
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            return (User) authentication.getPrincipal();
+        } else {
+            throw new AuthenticationCredentialsNotFoundException(
+                    "Current user has not authenticated with a valid account.");
+        }
+    }
+
+    public User getCurrentUser(Authentication authentication) {
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            return (User) authentication.getPrincipal();
+        } else {
+            throw new AuthenticationCredentialsNotFoundException(
+                    "Current user has not authenticated with a valid account.");
+        }
+    }
+}
