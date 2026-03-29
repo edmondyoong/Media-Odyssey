3374a2be16b4b02cda0063a8970a7491349f0cab
Edmond Yoong <edmond_yoong@sfu.ca>
2026-03-29 16:07:42 -0700

    feat: Added file upload to object storage

---
+++ src/main/java/com/mo/mediaodyssey/shared/services/ObjectStorageService.java
@@ -0,0 +1,66 @@
+package com.mo.mediaodyssey.shared.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.InputStream;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ObjectStorageService {
+
+    /**
+     * Service to access S3 compatible object storage solutions. Not limited to one
+     * object storage provider. Any S3 compayible object storage provider can be
+     * used.
+     * 
+     * For Media Odyssey, we have chosen Backblaze B2. Backblaze does not require
+     * payment information and provides 10GB for free. However, without payment,
+     * their buckets are private by default.
+     * However, using Cloudflare Workers, public requests to the Cloudflare Worker
+     * can sign requests using our Application Key to our private Backblaze bucket
+     * using the provided sample/guide:
+     * https://www.backblaze.com/docs/cloud-storage-deliver-private-backblaze-b2-content-through-cloudflare-cdn
+     * https://github.com/backblaze-b2-samples/cloudflare-b2/tree/main
+     * 
+     * Furthermore, using Backblaze and Cloudflare together results in free and
+     * unlimited egress.
+     */
+
+    private final S3Client s3Client;
+    private final String bucket;
+    private final String publicUrl;
+
+    public ObjectStorageService(S3Client s3Client,
+            @Value("${storage.bucket}") String bucket,
+            @Value("${storage.endpoint}") String endpoint,
+            @Value("${storage.public-url:}") String publicUrl) {
+        this.s3Client = s3Client;
+        this.bucket = bucket;
+        this.publicUrl = publicUrl;
+    }
+
+    public String uploadFile(MultipartFile file) {
+        String original = Optional.ofNullable(file.getOriginalFilename()).orElse("file");
+        String objectName = UUID.randomUUID() + "-" + original.replaceAll("\\s+", "_");
+
+        try (InputStream is = file.getInputStream()) {
+            PutObjectRequest putReq = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(objectName)
+                    .contentType(file.getContentType())
+                    .contentLength(file.getSize())
+                    .build();
+
+            s3Client.putObject(putReq, RequestBody.fromInputStream(is, file.getSize()));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to upload file", e);
+        }
+        return publicUrl.replaceAll("/$", "") + "/" + objectName;
+    }
+}
